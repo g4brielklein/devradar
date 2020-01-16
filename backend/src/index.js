2c391c24eda4992996f32eef199be76dd0fa32bc
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-01-16 14:51:01 -0300

    Criando a base da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://gabriel:7203001450@cluster0-9eike.mongodb.net/we
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
