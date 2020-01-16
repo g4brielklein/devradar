47f3c41578c5c1311f5f10e20e26b6d5449e317e
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-01-16 14:52:25 -0300

    Construindo a interface web

--- web/src/App.js
+++ web/src/App.js
@@ -1,142 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App() {
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+useEffect(() => {
+  async function loadDevs() {
+    const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    )
-  }, []);
+    setDevs(response.data);
+  }
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  loadDevs();
+}, []);
 
-    
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input 
-              name="github_username" 
-              id="github_username" 
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-              ></input>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-              name="techs" 
-              id="techs" 
-              required
-              value={techs}
-              onChange
-              onChange={e => setTechs(e.target.value)}
-              ></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number" 
-                name="latitude" 
-                id="latitude" 
-                required 
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-                ></input>
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type="number" 
-                name="longitude" 
-                id="longitude" 
-                required 
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-                ></input>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
+
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/22963494?s=460&v=4" alt="Gabriel Klein"></img>
-              <div className="user-info">
-                <strong>Gabriel Klein</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Análise e Desenvolvimento de Sistemas - ULBRA</p>
-            <a href="https://github.com/g4brielklein">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/22963494?s=460&v=4" alt="Gabriel Klein"></img>
-              <div className="user-info">
-                <strong>Gabriel Klein</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Análise e Desenvolvimento de Sistemas - ULBRA</p>
-            <a href="https://github.com/g4brielklein">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/22963494?s=460&v=4" alt="Gabriel Klein"></img>
-              <div className="user-info">
-                <strong>Gabriel Klein</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Análise e Desenvolvimento de Sistemas - ULBRA</p>
-            <a href="https://github.com/g4brielklein">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/22963494?s=460&v=4" alt="Gabriel Klein"></img>
-              <div className="user-info">
-                <strong>Gabriel Klein</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Análise e Desenvolvimento de Sistemas - ULBRA</p>
-            <a href="https://github.com/g4brielklein">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>    
